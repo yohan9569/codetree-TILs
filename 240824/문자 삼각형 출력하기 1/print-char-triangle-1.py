c9f773adadd97aa5fdd648d7de61cf18ef2dc38f
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-08-24 17:50:55 +0900

    Title: 문자 삼각형 출력하기 1, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240824/문자 삼각형 출력하기 1/print-char-triangle-1.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [[] for _ in range(n)]
+
+str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+idx = 0
+for i in range(n):
+    for j in range(i, n):
+        arr[j].append(str[idx])
+        idx += 1
+
+for a in arr:
+    print(" ".join(a).rjust(n*2-1))

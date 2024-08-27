9b5eddbaa9d48e3236aca73cdefabd6272a4cd66
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-08-27 14:11:05 +0900

    Title: 3 또는 5의 배수, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240827/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String ans;
+        if (a % 3 == 0) ans = "YES";
+        else ans = "NO";
+        System.out.println(ans);
+        if (a % 3 == 0) ans = "YES";
+        else ans = "NO";
+        System.out.println(ans);
+    }
+}

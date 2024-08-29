9dbe267b43d35295eb680dc041fa586a759a4879
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-08-29 09:20:24 +0900

    Title: 세 수의 중간값, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/세 수의 중간값/median-of-three-numbers.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt(), b=sc.nextInt(), c=sc.nextInt();
+        System.out.println((b>a && c>b) ? 1 : 0);
+    }
+}

ec1bb29480ac2c4a601405439cbbc9866b95f96f
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-11-23 23:10:40 +0900

    Title: 자연수면 출력, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241123/자연수면 출력/output-only-natural-number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+        if (a > 0) {
+            for (int i = 0; i < b; i++) System.out.print(a);
+        } else {
+            System.out.print(0);
+        }
+    }
+}

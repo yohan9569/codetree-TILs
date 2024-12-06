d3471e3ddf1cb9d022b11e10c42dc8398ee18940
Yohan Park <88149844+yohan9569@users.noreply.github.com>
2024-12-06 16:57:45 +0900

    Title: 체크 다이아몬드, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241206/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), i = 1;
+        for (; i<=n; i++) {
+            for (int j=n-i; j>0; j--) System.out.print(" ");
+            for (int j=0; j<i; j++) System.out.print("* ");
+            System.out.println();
+        }
+        for (i-=2; i>0; i--) {
+            for (int j=n-i; j>0; j--) System.out.print(" ");
+            for (int j=0; j<i; j++) System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
